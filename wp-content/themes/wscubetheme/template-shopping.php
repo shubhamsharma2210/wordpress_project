469efc9f7a70ae6317f8f60e3ecccd448793f6dd
shubham sharma <shubhamarela2001@gmail.com>
2025-02-25 13:55:13 +0530

    new

--- wp-content/themes/wscubetheme/template-shopping.php
+++ wp-content/themes/wscubetheme/template-shopping.php
@@ -2,7 +2,7 @@
 /*
 Template Name: shopping
 */
- get_header();
+get_header();
 ?>
 
 
@@ -22,18 +22,18 @@ Template Name: shopping
 <?php display_shopping_posts(); ?>
 
 
-
+// get new category
 <div class="news-categories">
-    <?php 
-    $cuisine = get_terms(['taxonomy' => 'cuisine', 'hide_empty' => false, 'orderby'=>'name','order'=>'ASC' ]);
-    foreach($cuisine as $getCuisinData){
-        ?>
+    <?php
+    $cuisine = get_terms(['taxonomy' => 'cuisine', 'hide_empty' => false, 'orderby' => 'name', 'order' => 'ASC']);
+    foreach ($cuisine as $getCuisinData) {
+    ?>
         <div class="categories">
             <a class="categories_chips" href="<?php echo (get_category_link($getCuisinData->term_id)); ?>"><?php echo esc_html($getCuisinData->name); ?></a>
         </div>
-   <?php } ?> 
-    </div>
-   
+    <?php } ?>
+</div>
+
 <?php display_restorant_post_type(); ?>
 
 
